fb5ccdddaec7f58c5ee96a368bb5febabb377e70
Ethan Glenn <ethan+git@eglenn.dev>
2026-07-18 17:41:30 -0600

    udpated footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
         <footer className="mt-8 border-t border-border/60 p-6 text-center text-[15px] text-muted-foreground">
-            &copy; {new Date().getFullYear()} | Not affiliated with any
-            religious organization.
+            <p>&copy; {new Date().getFullYear()} Gospel Help</p>
+            <p>Not affiliated with any religious organization.</p>
         </footer>
     );
 }
